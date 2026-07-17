8c29849350b1e90eb6f7a3e07328c38a8ce608b7
Benedict Meinke <Benedict_Meinke@web.de>
2026-07-17 16:50:22 +0200

    [ADD] a module.

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
-resource "aws_s3_bucket" "lab_bucket" {
-  bucket = local.name_prefix
+module "lab_bucket" {
+  source = "./modules/s3-bucket"
+
+  bucket_name = local.name_prefix
   tags = merge(local.common_tags, {
     CreatedInAccount = data.aws_caller_identity.current.account_id
   })
--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,7 @@
 output "bucket_name" {
-  description = "Name of the created S3 bucket"
-  value       = aws_s3_bucket.lab_bucket.id
+  value = module.lab_bucket.bucket_id
 }
 
 output "bucket_arn" {
-  description = "ARN of the created S3 bucket"
-  value       = aws_s3_bucket.lab_bucket.arn
-}
-
-output "aws_account_id" {
-  value = data.aws_caller_identity.current.account_id
-}
-
-output "current_region" {
-  value = data.aws_region.current.name
-}
-
-output "available_azs" {
-  value = data.aws_availability_zones.available.names
+  value = module.lab_bucket.bucket_arn
 }
