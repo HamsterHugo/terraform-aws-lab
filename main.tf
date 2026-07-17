47b44ed963d30db6f0b1cfe9131cf5f59437ed80
Benedict Meinke <Benedict_Meinke@web.de>
2026-07-17 16:57:02 +0200

    [ADD] a second bucket.

--- main.tf
+++ main.tf
@@ -6,3 +6,10 @@ module "lab_bucket" {
     CreatedInAccount = data.aws_caller_identity.current.account_id
   })
 }
+
+module "logs_bucket" {
+  source = "./modules/s3-bucket"
+
+  bucket_name = "${local.name_prefix}-logs"
+  tags = merge(local.common_tags, { Purpose = "logs" })
+}
