b60e5bd154b174d6ff8bf792172ef6601199f6cd
Benedict Meinke <Benedict_Meinke@web.de>
2026-07-17 15:32:01 +0200

    [ADD] Tags CreatedInAccount

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
 resource "aws_s3_bucket" "lab_bucket" {
   bucket = local.name_prefix
-  tags   = local.common_tags
+  tags   = merge(local.common_tags, {
+    CreatedInAccount = data.aws_caller_identity.current.account_id
+  })
 }
