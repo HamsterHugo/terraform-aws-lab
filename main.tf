4debc0ecab868df8198cb6b7053f219d4779199e
Benedict Meinke <Benedict_Meinke@web.de>
2026-07-17 16:28:06 +0200

    [FIX] format in main.tf.

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "lab_bucket" {
   bucket = local.name_prefix
-  tags   = merge(local.common_tags, {
+  tags = merge(local.common_tags, {
     CreatedInAccount = data.aws_caller_identity.current.account_id
   })
 }
