298058ce3698338330599bfa6dc0553f3b9a9d25
Benedict Meinke <Benedict_Meinke@web.de>
2026-07-17 15:25:58 +0200

    [ADD] data file.

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,15 @@ output "bucket_arn" {
   description = "ARN of the created S3 bucket"
   value       = aws_s3_bucket.lab_bucket.arn
 }
+
+output "aws_Account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "current_region" {
+  value = data.aws_region.current.name
+}
+
+output "available_azs" {
+  value = data.aws_availability_zones.available.names
+}
